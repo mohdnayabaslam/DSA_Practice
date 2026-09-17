28dc90f5a5ed9669566968668348db6b3def9a1a
Mohd Nayab Aslam <nayabaslam100@gmail.com>
2026-09-18 01:02:25 +0530

    Time: 3 ms (85.99%) | Memory: 43 MB (92.90%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> st = new Stack<>();
+        int i,len=s.length();
+        for(i=0;i<len;i++)
+        {
+            char ch=s.charAt(i);
+            if(ch=='(' || ch=='{' || ch=='[')
+                st.push(ch);
+            else
+            {
+                if(st.isEmpty())
+                    return false;
+                char top=st.peek();
+                if(ch==')' && top=='(' || ch=='}' && top=='{' || ch==']' && top=='[')
+                    st.pop();
+                else
+                    return false;
+            }
+        }
+        return st.isEmpty();
+    }
+}
